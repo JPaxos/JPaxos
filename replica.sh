859b3ec53173d889759fcc7a4372bd1cffd99a1a
Nuno Santos <nfssantos@gmail.com>
2011-04-19 13:24:40 +0000

    - Fixed typo on MinimumInstancesForSnapshotRatioSample property string. Was MinimumInstaqncesForSnapshotRatioSample - Improved: Added to paxos.properties the properties FDSuspectTimeout and FDSendTimeout, which control the failure detector. - Bug fix: FullLogFormatter: moved helper objects to thread local storage, avoid concurrent access to non-thread safe objects. - Simplification: Removed unnecessary sendLock from UDPNetwork.send(). The DatagramSocket.send is synchronized and all other variables used are local. - Improved: Startup sequence of threads and modules revised. All objects are initialized before their threads are started. The Dispatcher thread is started only after all modules finish initialization. - Bug fix: assertion error on view change if new leader had to fill empty consensus slots with no-op operation. This case was not implemented.

--- replica.sh
+++ replica.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -ea -Djava.util.logging.config.file=logging.properties -cp jpaxos-1.0rc1.jar lsr.paxos.test.EchoServer $*
+java -ea -Djava.util.logging.config.file=logging.properties -cp bin lsr.paxos.test.EchoServer $*
