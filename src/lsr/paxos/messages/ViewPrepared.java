c9342102c1c1918078f7ca29178a802609d6cde5
Nuno Santos <nfssantos@gmail.com>
2011-08-30 11:28:29 +0200

    Added missing classes to support request forwarding

---
+++ src/lsr/paxos/messages/ViewPrepared.java
@@ -0,0 +1,29 @@
+package lsr.paxos.messages;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+
+public class ViewPrepared extends Message {
+    private static final long serialVersionUID = 1L;
+    
+    public ViewPrepared(DataInputStream input) throws IOException {
+        super(input);
+    }
+    
+    public ViewPrepared(int view) {
+        super(view);
+    }
+
+    @Override
+    public MessageType getType() {
+        return MessageType.ViewPrepared;
+    }
+
+    @Override
+    protected void write(ByteBuffer bb) {
+        // Empty, nothing to write
+    }
+
+}
