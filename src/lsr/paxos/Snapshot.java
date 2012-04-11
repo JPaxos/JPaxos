f728e5d6fe47b919c29456636291d75689e29404
Lisa Nguyen Quang Do <tdlisanguyenqd001@gmail.com>
2012-04-11 10:49:21 +0200

    Working snapshot - To be tested

--- src/lsr/paxos/Snapshot.java
+++ src/lsr/paxos/Snapshot.java
@@ -9,24 +9,17 @@ public class Snapshot {
 
 	private byte[] data;
 	private Map<Long,Reply> lastReplyForClient;
-	private SnapshotHandle handle;
-	//private final int currentPaxosInstanceId;
+	private final SnapshotHandle handle;
 	
     public Snapshot(int paxosInstanceId, Map<Long,Reply> lastReplyForClient) { 
-		SnapshotHandle handle = new SnapshotHandle(paxosInstanceId);
+		this.handle = new SnapshotHandle(paxosInstanceId);
 		this.lastReplyForClient = lastReplyForClient;
-		//this.currentPaxosInstanceId = paxosInstanceId;
 	}
 	
 	public Snapshot(SnapshotHandle handle, Map<Long,Reply> lastReplyForClient) { 
 		this.handle = handle;
 		this.lastReplyForClient = lastReplyForClient;
-		//this.currentPaxosInstanceId = handle.getPaxosInstanceId();
 	}
-	
-	/*public int getPaxosInstanceId(){
-		return currentPaxosInstanceId;
-    }*/
 
 	public void setData(byte[] data){
 		this.data = data;
