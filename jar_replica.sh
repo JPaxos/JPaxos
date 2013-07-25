21f3061a791d4c6012248ee899b6ef2dae8b07a1
Jan Kończak <jan.konczak@cs.put.poznan.pl>
2013-07-25 15:45:55 +0200

    benchmarking fixes

--- jar_replica.sh
+++ jar_replica.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $( dirname $( readlink -f "$0" ) )
-java -ea -Djava.util.logging.config.file=logging.properties -cp jpaxos.jar lsr.paxos.test.EchoService $* 3>&1 1>&2 2>&3 3>&- | ./process.sh
+java -Djava.util.logging.config.file=logging.properties -cp jpaxos.jar lsr.paxos.test.EchoService $* 3>&1 1>&2 2>&3 3>&- | ./process.sh
