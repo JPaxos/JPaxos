3a8a5dba98b891b86dd3b69055a750e9f72e0320
Jan Kończak <jan.konczak@cs.put.poznan.pl>
2013-10-07 14:21:04 +0200

    Repeating tests from frida with TCP instead of NIO

---
+++ benchmark/varReqSizeTcp3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --wait-all-nodes=1 --nodelist="hpc-[2-8]" --time 01:00:00
+
+[[ `hostname` == hpc-2 ]] || exit 0
+
+out="`pwd`/${SLURM_JOB_NAME%.sh}.out"
+echo "# `date`" > $out
+
+cd ..
+
+for x in `seq 16 16 512 | awk '{for (i=0;i<3;++i) print;}' | shuf`
+do
+	rm -rf scenarios/temp
+	cp -r scenarios/nocrash scenarios/temp
+
+	cat scenarios/nocrash/scenario |\
+		sed "s/REQSIZE/$x/g" |\
+		sed "s/CLINO/333/g" |\
+		cat > scenarios/temp/scenario
+
+	cat scenarios/nocrash/paxos.properties |\
+		sed "s/WINSIZE/10/g" |\
+		sed "s/NETWORK/TCP/g" |\
+		cat > scenarios/temp/paxos.properties
+
+	NOCOLLECT=true SCENARIO=scenarios/temp ./runTestHpc.sh &>/dev/null # &>> ${out}.debug
+
+	rm -rf scenarios/temp
+
+	echo "$x $(ssh hpc-2 "cd /tmp/jpaxos_0 && grep 'RPS:' log.0 |\
+	     awk 'NR>50{sum+=\$3;count++};END{printf(\"%f\",sum/count);}'")" |\
+	     cat >> $out
+	     #tee -a $out
+done
